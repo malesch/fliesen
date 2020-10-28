688e97f0fab0b709a09e3a90465ead2cbcfa2922
Marcus Spiegel <malesch@gmail.com>
2020-10-28 12:46:59 +0100

    Output the  list pixel colors to stdout
    - Every pixel color on a line
    - A new pixel row is indicated by a line with an asterisk

--- main.go
+++ main.go
@@ -8,41 +8,12 @@ import (
 	"image/png"
 	"log"
 	"os"
-	"sort"
 )
 
 func init() {
 	image.RegisterFormat("gif", "gif", gif.Decode, gif.DecodeConfig)
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 	image.RegisterFormat("jpg", "jpg", jpeg.Decode, jpeg.DecodeConfig)
-}
-
-func totalCount(freqMap map[string]int) int {
-	total := 0
-	for _, v := range freqMap {
-		total = total + v
-	}
-	return total
-}
-
-func printTable(freqMap map[string]int) {
-	type keyValue struct {
-		Key   string
-		Value int
-	}
-
-	var kvSlice []keyValue
-	for k, v := range freqMap {
-		kvSlice = append(kvSlice, keyValue{k, v})
-	}
-
-	sort.Slice(kvSlice, func(i, j int) bool {
-		return kvSlice[i].Value > kvSlice[j].Value
-	})
-
-	for _, kv := range kvSlice {
-		fmt.Println(kv.Key, ": ", kv.Value)
-	}
 }
 
 func main() {
@@ -60,27 +31,16 @@ func main() {
 
 	img, _, err := image.Decode(reader)
 	bounds := img.Bounds()
-	fmt.Printf("Dimensions: width=%d, height=%d\n", (bounds.Max.X - bounds.Min.X), (bounds.Max.Y - bounds.Min.Y))
-
-	// Frequency map
-	pixelFrequency := make(map[string]int)
 
 	var color string
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			r, g, b, _ := img.At(x, y).RGBA()
 			color = fmt.Sprintf("%.2X%.2X%.2X", uint8(r), uint8(g), uint8(b))
-
-			_, ok := pixelFrequency[color]
-			if ok == true {
-				pixelFrequency[color]++
-			} else {
-				pixelFrequency[color] = 1
-			}
+			fmt.Printf("%s\n", color)
+		}
+		if y != (bounds.Max.Y - 1) {
+			fmt.Printf("*\n")
 		}
 	}
-
-	fmt.Printf("\nPixel color frequencies:\n")
-	printTable(pixelFrequency)
-	fmt.Printf("\nTotal number tiles: %d\n", totalCount(pixelFrequency))
 }
